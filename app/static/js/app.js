b6951326e017bff1c7a1521fd4456b03c3313dd3
Janne Spijkervet <janne.spijkervet@hotmail.com>
2018-05-19 11:46:59 +0200

    First commit

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,23 +1,40 @@
-/*
-    Boilerplate Python 3 SocketIO Flask app
-*/
+
+
+
+particlesJS.load('particles-js', '/static/assets/particles.json', function() {
+  console.log('particles.js loaded - callback');
+});
 
 var socket = io.connect('http://' + document.domain + ':' + location.port);
 socket.on('connect', function() {
-    socket.emit('connected', {data: 'Still connected'});
+  socket.emit('connected', {data: 'Still connected'});
 });
 
 socket.on('pong', function() {
-    socket.emit('ping', {data: 'Still connected'});
+  // socket.emit('ping', {data: 'Still connected'});
 });
 
 
 socket.on('update', function(data) {
-    update(JSON.parse(data));
+  update(JSON.parse(data));
 });
 
 
 function update(data) {
-    $("#version").text(data.version);
-    $("#time").text(data.time);
+  $("#version").text(data.version);
+  $("#time").text(data.time);
 }
+
+
+
+$(document).ready(function(){
+  var typed = new Typed('#typed', {
+    strings: ['Hi there!', 'Welcome to the website of the UvA Intelligent Robotics Lab'],
+    stringsElement: null,
+    typeSpeed: 10,
+    fadeOut: true,
+    fadeOutClass: 'typed-fade-out',
+    fadeOutDelay: 500,
+  });
+  console.log("TYPED");
+});
--- app/views/index.py
+++ app/views/index.py
@@ -6,6 +6,18 @@ index_bp = Blueprint('index', __name__)
 
 
 @index_bp.route('/', methods=['GET'])
-@login_required
+# @login_required
 def index():
     return render_template('index.html', app_info=app_info, user=current_user)
+
+@index_bp.route('/team', methods=['GET'])
+def team():
+    return render_template('team.html', app_info=app_info, user=current_user)
+
+@index_bp.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html', app_info=app_info, user=current_user)
+
+@index_bp.route('/papers', methods=['GET'])
+def papers():
+    return render_template('papers.html', app_info=app_info, user=current_user)
